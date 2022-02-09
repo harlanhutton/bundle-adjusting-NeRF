7a52f256fbd06118b2e9c9301f7e08c901e53c8a
HARLAN HUTTON <harlanrocks@gmail.com>
2022-02-09 15:29:21 -0500

    cpu usage

--- train.py
+++ train.py
@@ -15,18 +15,18 @@ def main():
     opt = options.set(opt_cmd=opt_cmd)
     options.save_options_file(opt)
 
-    with torch.cuda.device(opt.device):
+    #with torch.cuda.device(opt.device):
 
-        model = importlib.import_module("model.{}".format(opt.model))
-        m = model.Model(opt)
+    model = importlib.import_module("model.{}".format(opt.model))
+    m = model.Model(opt)
 
-        m.load_dataset(opt)
-        m.build_networks(opt)
-        m.setup_optimizer(opt)
-        m.restore_checkpoint(opt)
-        m.setup_visualizer(opt)
+    m.load_dataset(opt)
+    m.build_networks(opt)
+    m.setup_optimizer(opt)
+    m.restore_checkpoint(opt)
+    m.setup_visualizer(opt)
 
-        m.train(opt)
+    m.train(opt)
 
 if __name__=="__main__":
     main()
